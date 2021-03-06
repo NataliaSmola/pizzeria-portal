10e2bbae3cb013aa4b1483f1b634d69af59eec0a
Natalia <smolutka15@gmail.com>
2021-03-06 20:09:23 +0100

    Add components in view section

---
+++ src/components/views/Waiter/Waiter.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Waiter.module.scss';
+
+class Waiter extends React.Component {
+  render() {
+    return (
+      <div className={styles.component}>
+        <h2>Waiter view</h2>
+      </div>
+    );
+  }
+}
+
+export default Waiter;
